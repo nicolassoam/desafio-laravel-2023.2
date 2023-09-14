f4c8927932f48c4dbce83e520f4801ef15513ecb
nicolassoam <nsm4456@gmail.com>
2023-09-14 17:36:15 -0300

    primeiro commit

---
+++ database/migrations/2023_09_14_195101_create_consults_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consults', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('animal_id')->constrained()->onDelete('cascade');
+            $table->foreignId('owner_id')->constrained()->onDelete('cascade');
+            $table->string('treatment');
+            $table->double('cost');
+            $table->dateTime('treatment_start');
+            $table->dateTime('treatment_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consults');
+    }
+};
